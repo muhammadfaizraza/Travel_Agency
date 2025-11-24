10f5cfe25704c2b0948e19822aa69b0fe065f69e
muhammadfaizraza <muhammadfaizraza48@gmail.com>
2025-11-24 17:01:52 +0500

    cors issue

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -16,7 +16,7 @@ app.use(cors({
     'http://localhost:3000',
     'http://localhost:3001',
     'http://localhost:5173',
-    'https://travel-agency-test.vercel.app/login',
+    'https://travel-agency-test.vercel.app',
     process.env.FRONTEND_URL || ''
   ].filter(Boolean),
   credentials: true
